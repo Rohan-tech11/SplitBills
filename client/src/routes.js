cd17e536534ecaf1d5964d43289bc60b588f02ba
Rohan-tech11 <rohanreddy98765@gmail.com>
2024-02-16 02:05:41 -0500

    added register form

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,18 @@
 import { useRoutes } from "react-router-dom";
-//import LogoOnlyLayout from "./layouts/LogoOnlyLayout";
+import LogoOnlyLayout from "./layouts/LogoOnlyLayout";
 import configData from "./config.json";
 import Login from "./components/login";
+import Register from "./components/register";
 
 export default function Router() {
   return useRoutes([
     {
       path: configData.LOGIN_URL,
-      element: <Login />,
-      //children: [{ path: "", element: <Login /> }],
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: "", element: <Login /> },
+        { path: configData.REGISTER_URL, element: <Register /> },
+      ],
     },
   ]);
 }
