9546f95d64a07c2f1615014989a0063070869273
Rohan-tech11 <rohanreddy98765@gmail.com>
2024-03-09 15:33:58 -0500

    removed assets

--- client/src/theme/overrides/Card.js
+++ client/src/theme/overrides/Card.js
@@ -7,15 +7,15 @@ export default function Card(theme) {
         root: {
           boxShadow: theme.shadows[2],
           borderRadius: Number(theme.shape.borderRadius) * 2,
-          position: 'relative',
-          zIndex: 0, // Fix Safari overflow: hidden with border radius
+          position: "relative",
+          zIndex: 0,
         },
       },
     },
     MuiCardHeader: {
       defaultProps: {
-        titleTypographyProps: { variant: 'h6' },
-        subheaderTypographyProps: { variant: 'body2' },
+        titleTypographyProps: { variant: "h6" },
+        subheaderTypographyProps: { variant: "body2" },
       },
       styleOverrides: {
         root: {
